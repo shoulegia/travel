2b73d0e316c6754f4a046e4bffe066036f843056
wangjialing <wangjialing@sfhc.onaliyun.com>
2019-12-16 09:21:19 +0800

    补充

---
+++ travel/src/main/java/cn/itcast/travel/web/servlet/RegisterUserServlet.java
@@ -0,0 +1,52 @@
+package cn.itcast.travel.web.servlet;
+
+import cn.itcast.travel.domain.ResultInfo;
+import cn.itcast.travel.domain.User;
+import cn.itcast.travel.service.UserService;
+import cn.itcast.travel.service.impl.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+@WebServlet("/registerUserServlet")
+public class RegisterUserServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        Map<String, String[]> map = req.getParameterMap();
+
+        User user = new User();
+        try {
+            BeanUtils.populate(user, map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        UserService service = new UserServiceImpl();
+        boolean bl = service.register(user);
+        ResultInfo info = new ResultInfo();
+        if (bl) {
+            info.setFlag(true);
+        } else {
+            info.setFlag(false);
+            info.setErrorMsg("注册失败");
+        }
+        String json = new ObjectMapper().writeValueAsString(info);
+        resp.setContentType("application/json;charset=utf-8");
+        resp.getWriter().write(json);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
